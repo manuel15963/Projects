8473b6e7a3d693b140eb0e1c80dd9f4f9f19e084
adolfo <adolfo.berrocal@vallegrande.edu.pe>
2024-07-14 04:00:31 -0400

    microservicio de libros con kafka

--- src/main/java/com/example/Book_catalog_service/service/KafkaConsumerService.java
+++ src/main/java/com/example/Book_catalog_service/service/KafkaConsumerService.java
@@ -4,20 +4,20 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class KafkaConsumer {
+public class KafkaConsumerService {
 
     @KafkaListener(topics = "user-topic", groupId = "user-library-management-group")
-    public void consumeUserMessages(String message) {
+    public void consumeUserMessages(Object message) {
         System.out.println("Received user message: " + message);
     }
 
     @KafkaListener(topics = "book-topic", groupId = "book-catalog-service-group")
-    public void consumeBookMessages(String message) {
+    public void consumeBookMessages(Object message) {
         System.out.println("Received book message: " + message);
     }
 
     @KafkaListener(topics = "activity-topic", groupId = "activities-service-group")
-    public void consumeActivityMessages(String message) {
+    public void consumeActivityMessages(Object message) {
         System.out.println("Received activity message: " + message);
     }
 }
