15c47e9e3a2954ce0c830aa7e61662a845686733
adolfo <adolfo.berrocal@vallegrande.edu.pe>
2024-07-14 04:00:43 -0400

    microservicio  transaccional basica con kafka

--- src/main/java/com/example/user_activities/services/KafkaProducerService.java
+++ src/main/java/com/example/user_activities/services/KafkaProducerService.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducerService {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String topic, String message) {
+    public void sendMessage(String topic, Object message) {
         kafkaTemplate.send(topic, message);
     }
 }
